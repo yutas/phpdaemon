b4ce2092168e1c92d2c72b5833205d4369eed83e
Domashevich Artyom <a.domashevich@gmail.com>
2014-03-13 23:40:07 +0300

    got Daemon folder back instead of lib

--- examples/autoload.php
+++ examples/autoload.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function ($class) {
     $filename = dirname(__DIR__) . DIRECTORY_SEPARATOR
-                . str_replace("\\", DIRECTORY_SEPARATOR, preg_replace("/^Daemon/", 'lib', $class)) . '.php';
+                . str_replace("\\", DIRECTORY_SEPARATOR, $class) . '.php';
     if (file_exists($filename)) include $filename;
 });
 
